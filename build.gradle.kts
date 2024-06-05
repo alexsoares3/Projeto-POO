18807faf13155fc97d86279d8440951f60ec6992
woopz0 <alex.soares3@ua.pt>
2024-06-05 13:00:58 +0100

    Added Log4j2 dependency
    Added Log4j2 dependency. Fixed error that would come up when apache.pdfbox dependency was used

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,12 @@ repositories {
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     implementation("org.apache.poi:poi:5.2.3") //PPTX
-    implementation("org.apache.poi:poi-ooxml:5.2.3") //PPTX
     implementation("org.apache.pdfbox:pdfbox:3.0.2") //PDF
     implementation("org.xerial:sqlite-jdbc:3.36.0.3") //SQLite
-    implementation("org.apache.poi:poi-ooxml:5.2.4") //DOCX
+    implementation("org.apache.poi:poi-ooxml:5.2.4") //DOCX/PPTX
+    // Log4j2 Core
+    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.23.1")
 }
 
 tasks.test {
