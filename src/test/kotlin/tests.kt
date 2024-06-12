f8d41b4b1f12abdf4a3b8996a5ca2280bf621aae
Willdev1984 <101224462+Willdev1984@users.noreply.github.com>
2024-06-13 00:00:46 +0100

    Testes adicionados para outros tipos de arquivos

--- src/test/kotlin/tests.kt
+++ src/test/kotlin/tests.kt
@@ -1,6 +1,10 @@
 package org.example
 
+import FileDOCX
 import FilePDF
+import FilePPTX
+import FileTXT
+import createDirectories
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -9,18 +13,19 @@ import java.io.File
 
 class MainTest {
     @Test
-    fun createDirectories(): Unit {
+    fun createDirectoriesTest(): Unit {
 
-        val inputDir = File("filesToRead")
-        val outputDir = File("processedFiles")
-        val databaseDir = File("database")
+        val directories = createDirectories()
+        val inputDir = directories["input"]
+        val outputDir = directories["output"]
+        val databaseDir = directories["database"]
 
-        createDirectories()
 
-        assertTrue(inputDir.exists())
-        assertTrue(outputDir.exists())
-        assertTrue(databaseDir.exists())
+        assertTrue(inputDir!!.exists())
+        assertTrue(outputDir!!.exists())
+        assertTrue(databaseDir!!.exists())
     }
+
 
     @Test
     fun processPDFFile(): Unit {
@@ -35,5 +40,50 @@ class MainTest {
 
         assertEquals(wordList, filePDF.wordList)
         assertTrue(filePDF.processed)
+    }
+
+    @Test
+    fun processTXTFile(): Unit{
+
+        val filepath = this::class.java.classLoader.getResource("txt_test.txt")
+        val file = File(filepath.file)
+        val wordList = mapOf("lets" to 1, "test" to 1, "reading" to 1, "txt" to 1, "files" to 1, "hello" to 2, "world" to 2)
+        val process = false
+        val fileTXT = FileTXT(file, wordList, process)
+
+        fileTXT.readFile()
+
+        assertEquals(wordList, fileTXT.wordList)
+        assertTrue(fileTXT.processed)
+    }
+
+    @Test
+    fun processPPTXFile(): Unit{
+
+        val filepath = this::class.java.classLoader.getResource("pptx_test.pptx")
+        val file = File(filepath.file)
+        val wordList = mapOf("lets" to 1, "test" to 1, "reading" to 1, "pptx" to 1, "files" to 1, "hello" to 2, "world" to 2)
+        val process = false
+        val filePPTX = FilePPTX(file, wordList, process)
+
+        filePPTX.readFile()
+
+        assertEquals(wordList, filePPTX.wordList)
+        assertTrue(filePPTX.processed)
+    }
+
+    @Test
+    fun processDocx(): Unit {
+
+        val filepath = this::class.java.classLoader.getResource("docx_test.docx")
+        val file = File(filepath.file)
+        val wordList = mapOf("lets" to 1, "test" to 1, "reading" to 1, "docx" to 1, "files" to 1, "hello" to 2, "world" to 2)
+        val process = false
+        val fileDOCX = FileDOCX(file, wordList, process)
+
+        fileDOCX.readFile()
+
+        assertEquals(wordList, fileDOCX.wordList)
+        assertTrue(fileDOCX.processed)
     }
 }
