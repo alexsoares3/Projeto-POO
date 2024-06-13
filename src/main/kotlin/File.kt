dc8be5146db47fd332c51ff1fca11e7e962d8ec7
woopz0 <alex.soares3@ua.pt>
2024-06-13 12:39:39 +0100

    Improved filter for processWords
    - Replaced "-" with "" in words that end up cut up by line changes ex "hello-" becomes "hello"
    - Filtered out words like "-los, -nos" that end up split from the full word

--- src/main/kotlin/File.kt
+++ src/main/kotlin/File.kt
@@ -15,8 +15,14 @@ abstract class File_base(var path: File, var wordList: Map<String, Int>?, var pr
             val words = line.split("[^\\w-]+".toRegex()) // Split by non-word characters
 
             for (word in words) {
-                val sanitizedWord = word.trim().lowercase() // Remove whitespace and convert to lowercase
+                var sanitizedWord = word.trim().lowercase() // Remove whitespace and convert to lowercase
                 if (sanitizedWord.isNotEmpty() && sanitizedWord.length > 2) { // Check if it's not empty and over 2 chars
+                    if ("-" in sanitizedWord && sanitizedWord.length <= 4 || sanitizedWord.all { it == '-' }) { // Filter out words that look something like "-los, -nos" or "----"
+                        continue
+                    }
+                    if (sanitizedWord.last().toString() == "-" || sanitizedWord.first().toString() == "-" ) { // Replace - with nothing if it's at the start or end
+                        sanitizedWord = sanitizedWord.replace("-", "")                       // For example "Hello-" becomes "Hello"
+                    }
                     if (!wordCountMap.containsKey(sanitizedWord)) { // Check if the word is already in the dictionary, if not add it and set its value to 1
                         wordCountMap[sanitizedWord] = 1
                     } else {
