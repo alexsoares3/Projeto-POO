96152fc95bee9158566cf5a457c503bddd9f9f39
JaiBand <161451469+JaiBand@users.noreply.github.com>
2024-06-14 16:30:21 +0100

    Função para verificar se a db existe e implementação nas funções searchDB,resetDB e wordCloud

--- src/main/kotlin/Database.kt
+++ src/main/kotlin/Database.kt
@@ -30,7 +30,6 @@ fun createTables() {
             name TEXT
         )
     """.trimIndent())
-
     statement.executeUpdate("""
         CREATE TABLE IF NOT EXISTS file_words (
             file_id INT,
@@ -39,7 +38,6 @@ fun createTables() {
             FOREIGN KEY (file_id) REFERENCES files(id)
         )
     """.trimIndent())
-
     statement.close()
     // Close connection
     connection.close()
@@ -138,161 +136,182 @@ fun getAllFiles(): List<Pair<String, Int>> {
     return fileList
 }
 
+fun checkIfDBExists(): Boolean {
+    val dbFilePath : File?
+    try {
+        dbFilePath = File(createDirectories()["database"]!!.absolutePath + "/database.db")
+    } catch (e: SQLException) {
+        e.printStackTrace()
+        return false
+    }
+    return dbFilePath.exists()
+}
+
 // This function is used to search for multiple words in the files.
 // It takes a string of words as input, splits it into a list of words, and then searches for each word in the files.
 fun searchDB(words: String): List<Triple<String, List<String>, Int>> {
+    if (checkIfDBExists()) {
+        // Return empty list if input is empty
+        // Not sure if necessary, since it might be useful to check what files are stored
+        if (words.isBlank()) {
+            return emptyList()
+        }
 
-    // Return empty list if input is empty
-    // Not sure if necessary, since it might be useful to check what files are stored
-    if (words.isBlank()) {
-        return emptyList()
-    }
+        // Split the input string into a list of words
+        val wordList = words.split(" ")
 
-    // Split the input string into a list of words
-    val wordList = words.split(" ")
+        // Get a connection to the database
+        val connection = getConnection()
 
-    // Get a connection to the database
-    val connection = getConnection()
+        // List to store the search results
+        val fileList = mutableListOf<Triple<String, List<String>, Int>>()
 
-    // List to store the search results
-    val fileList = mutableListOf<Triple<String, List<String>, Int>>()
-
-    // For each word in the wordList, create a subquery that selects the names of files that contain the word
-    val subQueries = wordList.map { word ->
-        """
+        // For each word in the wordList, create a subquery that selects the names of files that contain the word
+        val subQueries = wordList.map { word ->
+            """
         SELECT f.name, w.word, w.count
         FROM files AS f
         LEFT JOIN file_words AS w ON f.id = w.file_id
         WHERE w.word LIKE '%$word%'
         """.trimIndent()
-    }
-
-    // Join the subqueries using union to get the names of files that contain any of the words in the wordList
-    val sql = subQueries.joinToString(" UNION ")
-
-    // Prepare the SQL statement
-    val statement: PreparedStatement = connection.prepareStatement(sql)
-
-    // TODO Add try catch here
-    // Start transaction
-    connection.autoCommit = false
-    // Execute the SQL query and get the result set
-    val resultSet: ResultSet = statement.executeQuery()
-
-
-    // Store the names of the files that contain all the words (or similar) in the wordList
-    val fileWordsMap = mutableMapOf<String, Pair<MutableList<String>, Int>>()
-
-    // Iterate over the result set and add the names of the files and the words to the fileWordsMap
-    while (resultSet.next()) {
-        // Get the necessary values from the result set
-        val path = resultSet.getString("name")
-        val word = resultSet.getString("word")
-        val count = resultSet.getInt("count")
-
-        // Get the current pair from the fileWordsMap or create a new pair if it doesn't exist
-        var currentPair = fileWordsMap.getOrPut(path) { Pair(mutableListOf(), 0) }
-
-        // Add the word to the list of words and update the count
-        currentPair.first.add(word)
-        currentPair = Pair(currentPair.first, currentPair.second + count)
-
-        // Update the filewordsmap with the new pair
-        fileWordsMap[path] = currentPair
-    }
-
-    // Close the result set and the statement
-    resultSet.close()
-    statement.close()
-    // Close connection
-    connection.close()
-
-    // Iterate over the fileWordsMap and add the file names and the wordList to the fileList
-    for ((fileName, pair) in fileWordsMap) {
-        // Check if there are the same amount or more of words found and words searched
-        if (pair.first.size >= wordList.size) {
-            fileList.add(Triple(fileName, pair.first, pair.second))
         }
+
+        // Join the subqueries using union to get the names of files that contain any of the words in the wordList
+        val sql = subQueries.joinToString(" UNION ")
+
+        // Prepare the SQL statement
+        val statement: PreparedStatement = connection.prepareStatement(sql)
+
+        // TODO Add try catch here
+        // Start transaction
+        connection.autoCommit = false
+        // Execute the SQL query and get the result set
+        val resultSet: ResultSet = statement.executeQuery()
+
+
+        // Store the names of the files that contain all the words (or similar) in the wordList
+        val fileWordsMap = mutableMapOf<String, Pair<MutableList<String>, Int>>()
+
+        // Iterate over the result set and add the names of the files and the words to the fileWordsMap
+        while (resultSet.next()) {
+            // Get the necessary values from the result set
+            val path = resultSet.getString("name")
+            val word = resultSet.getString("word")
+            val count = resultSet.getInt("count")
+
+            // Get the current pair from the fileWordsMap or create a new pair if it doesn't exist
+            var currentPair = fileWordsMap.getOrPut(path) { Pair(mutableListOf(), 0) }
+
+            // Add the word to the list of words and update the count
+            currentPair.first.add(word)
+            currentPair = Pair(currentPair.first, currentPair.second + count)
+
+            // Update the filewordsmap with the new pair
+            fileWordsMap[path] = currentPair
+        }
+
+        // Close the result set and the statement
+        resultSet.close()
+        statement.close()
+        // Close connection
+        connection.close()
+
+        // Iterate over the fileWordsMap and add the file names and the wordList to the fileList
+        for ((fileName, pair) in fileWordsMap) {
+            // Check if there are the same amount or more of words found and words searched
+            if (pair.first.size >= wordList.size) {
+                fileList.add(Triple(fileName, pair.first, pair.second))
+            }
+        }
+
+        // Sort filelist based on word count (highest to lowest)
+        fileList.sortByDescending { it.third }
+
+        // Return the fileList
+        return fileList
+    }else{
+        println("Database not found")
+        return emptyList()
     }
-
-    // Sort filelist based on word count (highest to lowest)
-    fileList.sortByDescending { it.third }
-
-    // Return the fileList
-    return fileList
 }
+
 
 // This function resets the database, it also gives the user the option to delete the files in the output directory
 // or simply move them back to input folder
 fun resetDB() {
-    val connection = getConnection()
-    val sqlFiles = "DELETE FROM files"
-    val sqlFileWords = "DELETE FROM file_words"
+    if(checkIfDBExists()) {
+        val connection = getConnection()
+        val sqlFiles = "DELETE * FROM files IF EXISTS"
+        val sqlFileWords = "DELETE * FROM file_words IF EXISTS"
 
-    val statementFiles: Statement = connection.createStatement()
-    val statementFileWords: Statement = connection.createStatement()
+        val statementFiles: Statement = connection.createStatement()
+        val statementFileWords: Statement = connection.createStatement()
 
-    statementFiles.executeUpdate(sqlFiles)
-    statementFileWords.executeUpdate(sqlFileWords)
+        statementFiles.executeUpdate(sqlFiles)
+        statementFileWords.executeUpdate(sqlFileWords)
 
-    statementFiles.close()
-    statementFileWords.close()
-    connection.close()
-    println("Database reset.")
+        statementFiles.close()
+        statementFileWords.close()
+        connection.close()
+        println("Database reset.")
 
-    // Confirm if user wants to delete files
-    var answer = ""
-    while (answer != "y" && answer != "n") {
-        print("Do you want to delete the files? (y/n) ")
-        answer = readln().lowercase(Locale.getDefault())
-    }
-    val directories = createDirectories()
-    if (answer == "y") {
-        val outputDir = directories["output"]
-        var fileCounter = 0
-        outputDir?.listFiles()?.forEach { file ->
+        // Confirm if user wants to delete files
+        var answer = ""
+        while (answer != "y" && answer != "n") {
+            print("Do you want to delete the files? (y/n) ")
+            answer = readln().lowercase(Locale.getDefault())
+        }
+        val directories = createDirectories()
+        if (answer == "y") {
+            val outputDir = directories["output"]
+            var fileCounter = 0
+            outputDir?.listFiles()?.forEach { file ->
                 try {
                     file.delete()
                     fileCounter++
                 } catch (e: FileNotFoundException) {
                     println("File not found: ${file}.")
-                }   catch (e: Exception) {
+                } catch (e: Exception) {
                     e.printStackTrace()
                 }
-        }
-        println("Deleted $fileCounter file(s).")
-    } else {
-        // Move files back to input directory so they can be processed again.
-        val outputDir = directories["output"]
-        val inputDir = directories["input"]
-        var fileCounter = 0
-        outputDir?.listFiles()?.forEach { file ->
-            val sourceFile = file.toPath()
-            val destinationFile = File(inputDir, sourceFile.fileName.toString())
-            try {
-                Files.move(sourceFile, destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
-                fileCounter++
-            } catch (e: FileNotFoundException) {
-                println("File not found: ${sourceFile}.")
-            } catch (e: Exception) {
-                e.printStackTrace()
             }
+            println("Deleted $fileCounter file(s).")
+        } else {
+            // Move files back to input directory so they can be processed again.
+            val outputDir = directories["output"]
+            val inputDir = directories["input"]
+            var fileCounter = 0
+            outputDir?.listFiles()?.forEach { file ->
+                val sourceFile = file.toPath()
+                val destinationFile = File(inputDir, sourceFile.fileName.toString())
+                try {
+                    Files.move(sourceFile, destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+                    fileCounter++
+                } catch (e: FileNotFoundException) {
+                    println("File not found: ${sourceFile}.")
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
 
+            }
+            println("Moved $fileCounter file(s) back to input directory.")
         }
-        println("Moved $fileCounter file(s) back to input directory.")
+    } else{
+        println("Database not found.")
     }
 }
 
 // wordCloud returns the x amount of most common words
 fun wordCloud(nrWords: String): List<Pair<String, Int>> {
-    val nrWordsInt: Int = try {
-        nrWords.toInt()
-    } catch (e: NumberFormatException) {
-        println("Invalid input. Please enter a number.")
-        return emptyList()
-    }
-    val connection = getConnection()
-    val sql = """
+    if (checkIfDBExists()){
+        val nrWordsInt: Int = try {
+            nrWords.toInt()
+        } catch (e: NumberFormatException) {
+            println("Invalid input. Please enter a number.")
+            return emptyList()
+        }
+        val connection = getConnection()
+        val sql = """
         SELECT word, COUNT(*) as totalCount
         FROM file_words
         GROUP BY word
@@ -300,21 +319,25 @@ fun wordCloud(nrWords: String): List<Pair<String, Int>> {
         LIMIT ?
     """.trimIndent()
 
-    val statement: PreparedStatement = connection.prepareStatement(sql)
-    statement.setInt(1, nrWordsInt)
+        val statement: PreparedStatement = connection.prepareStatement(sql)
+        statement.setInt(1, nrWordsInt)
 
-    val resultSet: ResultSet = statement.executeQuery()
-    val commonWords = mutableListOf<Pair<String, Int>>()
+        val resultSet: ResultSet = statement.executeQuery()
+        val commonWords = mutableListOf<Pair<String, Int>>()
 
-    while (resultSet.next()) {
-        val word = resultSet.getString("word")
-        val totalCount = resultSet.getInt("totalCount")
-        commonWords.add(word to totalCount)
+        while (resultSet.next()) {
+            val word = resultSet.getString("word")
+            val totalCount = resultSet.getInt("totalCount")
+            commonWords.add(word to totalCount)
+        }
+
+        resultSet.close()
+        statement.close()
+        connection.close()
+
+        return commonWords
+    } else{
+        println("Database not found.")
+        return emptyList()
     }
-
-    resultSet.close()
-    statement.close()
-    connection.close()
-
-    return commonWords
 }
